8c782bee5e794a43b2fb12e82f38b4f8be94d7c3
Stefan Hamburger <9825318+stefanhamburger@users.noreply.github.com>
2019-10-17 17:19:00 +0200

    🎨 Do not render empty table while JSON is loading

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import getJson from './jsonLoader';
 import { setState } from './state';
 
 (async function() {
-  render();
-
   const logEntries = await getJson();
   setState(logEntries);
   render();
