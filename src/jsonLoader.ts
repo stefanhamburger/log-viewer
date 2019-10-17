3602652d444dac8a90e67620f9174c90269429d4
Stefan Hamburger <9825318+stefanhamburger@users.noreply.github.com>
2019-10-17 15:29:29 +0200

    ✨ Implement JSON loader

--- src/jsonLoader.ts
+++ src/jsonLoader.ts
@@ -1,5 +1,20 @@
 import LogEntry from "./LogEntry";
 
-export default function getJson(): Promise<LogEntry[]> {
-  return new Promise((resolve) => resolve([]));
+interface RawLogEntry {
+  message?: string;
+  facility?: string;
+  level?: "Notice" | "Debug" | string;
+  timeStamp?: string;
+}
+
+export default async function getJson(): Promise<LogEntry[]> {
+  const fileContents = await fetch("./errors.json");
+  const json = await fileContents.json() as { data: RawLogEntry[] };
+
+  return json.data.map(({ message, facility, level, timeStamp }) => ({
+    message,
+    facility,
+    level,
+    timeStamp: timeStamp === undefined ? undefined : new Date(timeStamp),
+  }));
 }
