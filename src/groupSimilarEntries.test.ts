c8f90b4cc9f6cb89eb95c3296eb8eda92d96b09c
Stefan Hamburger <9825318+stefanhamburger@users.noreply.github.com>
2019-10-17 17:04:41 +0200

    ✔️ Add test for groupSimilarEntries

---
+++ src/groupSimilarEntries.test.ts
@@ -0,0 +1,56 @@
+import LogEntry from "./interfaces/LogEntry";
+import RawLogEntry from "./interfaces/RawLogEntry";
+import groupSimilarEntries from "./groupSimilarEntries";
+
+const testDate = new Date();
+
+const input: RawLogEntry[] = [
+  {
+    level: "Notice",
+    message: "First message",
+    facility: "HQ",
+    timeStamp: testDate.toISOString(),
+  },
+  {
+    level: "Debug",
+    message: "Second message",
+    facility: "HQ",
+    timeStamp: testDate.toISOString(),
+  },
+  {
+    level: "Debug",
+    message: "Third message",
+    facility: "HQ",
+    timeStamp: testDate.toISOString(),
+  },
+  {
+    level: "Debug",
+    message: "Fourth message",
+    facility: "Branch",
+    timeStamp: testDate.toISOString(),
+  },
+];
+const output: LogEntry[] = [
+  {
+    level: "Notice",
+    message: "First message",
+    facility: "HQ",
+    timeStamp: testDate,
+  },
+  {
+    level: "Debug",
+    message: "Second message\nThird message",
+    facility: "HQ",
+    timeStamp: testDate,
+  },
+  {
+    level: "Debug",
+    message: "Fourth message",
+    facility: "Branch",
+    timeStamp: testDate,
+  },
+];
+
+test("groups 3 entries into 2", () => {
+  expect(groupSimilarEntries(input)).toEqual(output);
+})
