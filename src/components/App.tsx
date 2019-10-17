b093a080a3048a3f29adaa757a043dc0df0c643f
Stefan Hamburger <9825318+stefanhamburger@users.noreply.github.com>
2019-10-17 17:18:06 +0200

    🚧 Add filter header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,6 +12,10 @@ export default function App({ logEntries }: { logEntries: LogEntry[] }): JSX.Ele
       ? <Text block={true}>No log entries found.</Text>
       : (<>
         <Text block={true}>Found {numLogEntries} log entries.</Text>
+        <p><label>Filter by name: <input type="search" /></label></p>
+        <p><label>Level: <select><option defaultChecked>All</option><option>Notice</option><option>Debug</option></select></label></p>
+        <p><label>Facility: <select><option defaultChecked>All</option><option>GF::afml</option><option>GF::eai:eproduct</option></select></label></p>
+        <p><label>Time display: <select><option defaultChecked>Relative</option><option>Absolute</option></select></label></p>
         <Table logEntries={logEntries} />
       </>)}
   </>);
